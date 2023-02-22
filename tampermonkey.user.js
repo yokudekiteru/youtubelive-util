44347c863bd86f3c1ef272cfe8dc92a8ceffde77
Takashi OGAWA <ogw.tttt@gmail.com>
2023-02-22 18:45:04 +0900

    広告自動挿入のUI変更

--- tampermonkey.user.js
+++ tampermonkey.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YouTubeLiveユーティリティ
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.4
 // @description  YouTubeStudioでのライブ配信を補助するツール
 // @author       You
 // @match        https://studio.youtube.com/video/*/livestreaming*
@@ -10,19 +10,19 @@
 // ==/UserScript==
 
 /**
- * ミッドロール広告を挿入したいライブ配信画面のタブを開きっぱなしにしておけば、定期的に広告挿入ボタンをクリックする
- * 配信開始と終了の予約を可能とする（予約ボタン押下後は画面を開きっぱなしにしておくこと）。
+ * 画面を開きっぱなしにしておくことで、時間差で画面操作をおこなうツールです
+ * [できること]
+ * - 定期的に広告挿入ボタンをクリックする
+ * - 配信開始と終了の予約を可能とする
  */
-
-const DEFAULT_INTERVAL_MINUTES = 15;
 
 (function() {
   'use strict';
 
   const insertAdButtonId = 'insert-ad-button';
   let insertAdNextTimeoutId = null;
+  const insertAdBaseDatetimeId = 'ytlutil-insert-ad-base-datetime';
   const insertAdSwitchId = 'ytlutil-insert-ad-switch';
-  const insertAdOnAlertId = 'ytlutil-insert-ad-on-alert';
   const insertAdExecMonitorId = 'ytlutil-insert-ad-exec-monitor';
   const insertAdLastExecDatetimeId = 'ytlutil-insert-ad-last-exec-datetime';
   const insertAdNextExecDatetimeId = 'ytlutil-insert-ad-next-exec-datetime';
@@ -46,6 +46,7 @@ const DEFAULT_INTERVAL_MINUTES = 15;
       console.log('処理実行');
       insertAdButton.click();
       document.getElementById(insertAdLastExecDatetimeId).innerText = new Date().toLocaleString();
+      document.getElementById(insertAdSwitchId).disabled = 'disabled';
       scheduleNextInsertAd();
     } else {
       // 自動挿入を強制オフにする
@@ -56,11 +57,26 @@ const DEFAULT_INTERVAL_MINUTES = 15;
 
   const scheduleNextInsertAd = function() {
 
-    const myFlag = document.getElementById(insertAdSwitchId).value === 'on';
+    const myFlag = document.getElementById(insertAdSwitchId).value !== '-1';
 
     let now = new Date();
+    let baseDatetimeString = document.getElementById(insertAdBaseDatetimeId).innerText;
+    if (baseDatetimeString === '--') {
+      return;
+    }
+
+    let baseDatetime = new Date(baseDatetimeString);
+    let mySwitch = document.getElementById(insertAdSwitchId).value;
+
+    mySwitch = 1 * mySwitch * 60 * 1000;
     let myInterval = (1 * document.getElementById(insertAdIntervalMinutesId).value) * 60 * 1000;
+    let firstExecDatetime = new Date(baseDatetime.getTime() + mySwitch);
+    console.log(firstExecDatetime.toLocaleString());
+    let lastExecDatetime = document.getElementById(insertAdLastExecDatetimeId).innerText;
     let nextExecDatetime = new Date(now.getTime() + myInterval);
+    if (lastExecDatetime === '未実行' && mySwitch !== 0 && firstExecDatetime > now) {
+      nextExecDatetime = firstExecDatetime;
+    }
 
     // 次回実行をキャンセル
     if (insertAdNextTimeoutId !== null) {
@@ -69,23 +85,18 @@ const DEFAULT_INTERVAL_MINUTES = 15;
     }
 
     if (myFlag) {
-      // 「タブを閉じないで！」を表示
-      document.getElementById(insertAdOnAlertId).style.display = 'inline';
       // 次回実行予定を記入
       document.getElementById(insertAdNextExecDatetimeId).innerText = nextExecDatetime.toLocaleString();
       // 次回実行を登録
-      console.log('次回実行を登録: interval=' + myInterval);
-      insertAdNextTimeoutId = setTimeout(insertMidRollAd, myInterval);
+      insertAdNextTimeoutId = setTimeout(insertMidRollAd, nextExecDatetime.getTime() - now.getTime());
       console.log('次回実行: ' + insertAdNextTimeoutId);
     } else {
-      // 「タブを閉じないで！」を非表示
-      document.getElementById(insertAdOnAlertId).style.display = 'none';
       // 次回実行予定を記入
       document.getElementById(insertAdNextExecDatetimeId).innerText = ' - ';
     }
   }
 
-  const scheduleStream = function(startEnd, dateId, timeId, reserveButtonId, clickButtonId, dialogButtonSelector) {
+  const scheduleStream = function(startEnd, dateId, timeId, reserveButtonId, clickButtonId, callback) {
     const scheduleDate = document.getElementById(dateId);
     const scheduleTime = document.getElementById(timeId);
     if (!scheduleDate.value || !scheduleTime.value) {
@@ -104,16 +115,21 @@ const DEFAULT_INTERVAL_MINUTES = 15;
     document.getElementById(reserveButtonId).innerText = '変更';
     return setTimeout(function() {
       document.getElementById(clickButtonId).click();
-      if (dialogButtonSelector) {
-        setTimeout(function() {
-          document.querySelector(dialogButtonSelector).click();
-        }, 3000);
+      if (typeof callback === 'function') {
+        callback();
       }
     }, delay);
   }
 
   const scheduleStartStream = function() {
-    let newTimeoutId = scheduleStream('開始', startStreamDateId, startStreamTimeId, startStreamReserveButtonId, startStreamButtonId, '');
+    let newTimeoutId = scheduleStream(
+      '開始', startStreamDateId, startStreamTimeId, startStreamReserveButtonId, startStreamButtonId,
+      function() {
+        // 広告自動挿入の基準日時を設定し、広告自動挿入を起動
+        document.getElementById(insertAdBaseDatetimeId).innerText = new Date().toLocaleString();
+        scheduleNextInsertAd();
+      }
+    );
     if (startStreamTimeoutId !== null) {
       clearTimeout(startStreamTimeoutId);
     }
@@ -121,7 +137,14 @@ const DEFAULT_INTERVAL_MINUTES = 15;
   }
 
   const scheduleEndStream = function() {
-    let newTimeoutId = scheduleStream('終了', endStreamDateId, endStreamTimeId, endStreamReserveButtonId, endStreamButtonId, '#end-stream-confirmation #confirm-button');
+    let newTimeoutId = scheduleStream(
+      '終了', endStreamDateId, endStreamTimeId, endStreamReserveButtonId, endStreamButtonId,
+      function() {
+        setTimeout(function() {
+          document.querySelector('#end-stream-confirmation #confirm-button').click();
+        }, 3000);
+      }
+    );
     if (endStreamTimeoutId !== null) {
       clearTimeout(endStreamTimeoutId);
     }
@@ -129,35 +152,79 @@ const DEFAULT_INTERVAL_MINUTES = 15;
   }
 
   const initMe = function() {
+    let now = new Date();
+    const nowOnAir = document.getElementById(startStreamButtonId).ariaDisabled === 'true';
     const displayInsertAdEl = document.createElement('span');
-    displayInsertAdEl.innerHTML = `ミッドロール広告自動挿入
-    <select id="${insertAdSwitchId}"><option value="on">on</option><option value="off">off</option></select>
-    <span id="${insertAdOnAlertId}">タブを閉じないで！</span>
-    <br />
-    <span id="${insertAdExecMonitorId}" style="font-size: 0.8em;">
-    実行間隔: <input type="number" id="${insertAdIntervalMinutesId}" value="${DEFAULT_INTERVAL_MINUTES}" style="width: 3em;" />分
-    　次回予定: <span id="${insertAdNextExecDatetimeId}">未実行</span>
-    　最終実行: <span id="${insertAdLastExecDatetimeId}">未実行</span>
-    </span>`;
+    displayInsertAdEl.innerHTML = `自動広告 起点: <span id="${insertAdBaseDatetimeId}">--</span>
+&nbsp;初回:<select id="${insertAdSwitchId}">
+<option value="90">90分後</option>
+<option value="85">85分後</option>
+<option value="70">70分後</option>
+<option value="65">65分後</option>
+<option value="60" selected>60分後</option>
+<option value="55">55分後</option>
+<option value="50">50分後</option>
+<option value="45">45分後</option>
+<option value="40">40分後</option>
+<option value="35">35分後</option>
+<option value="30">30分後</option>
+<option value="25">25分後</option>
+<option value="20">20分後</option>
+<option value="15">15分後</option>
+<option value="10">10分後</option>
+<option value="5">5分後</option>
+<option value="1">1分後</option>
+<option value="-1">off</option>
+</select>
+<br />
+<span id="${insertAdExecMonitorId}" style="font-size: 0.8em;">
+実行間隔:<select id="${insertAdIntervalMinutesId}">
+<option value="120">120分</option>
+<option value="115">115分</option>
+<option value="110">110分</option>
+<option value="105">105分</option>
+<option value="100">100分</option>
+<option value="95">95分</option>
+<option value="90">90分</option>
+<option value="85">85分</option>
+<option value="70">70分</option>
+<option value="65">65分</option>
+<option value="60">60分</option>
+<option value="55">55分</option>
+<option value="50">50分</option>
+<option value="45">45分</option>
+<option value="40">40分</option>
+<option value="35">35分</option>
+<option value="30">30分</option>
+<option value="25">25分</option>
+<option value="20">20分</option>
+<option value="15" selected>15分</option>
+<option value="10">10分</option>
+<option value="5">5分</option>
+</select>
+　前回: <span id="${insertAdLastExecDatetimeId}">未実行</span>
+　次回: <span id="${insertAdNextExecDatetimeId}">未実行</span>
+</span>`;
     displayInsertAdEl.style.fontSize = '1.5em';
     displayInsertAdEl.style.fontWeight = 'bold';
     displayInsertAdEl.style.color = 'yellow';
     document.querySelector('.ytls-header.action-buttons').prepend(displayInsertAdEl);
+    if (nowOnAir) {
+      document.getElementById(insertAdBaseDatetimeId).innerText = now.toLocaleString();
+    }
     document.getElementById(insertAdSwitchId).addEventListener('change', scheduleNextInsertAd);
     document.getElementById(insertAdIntervalMinutesId).addEventListener('change', scheduleNextInsertAd);
-
     scheduleNextInsertAd();
 
     const displayStreamTimerEl = document.createElement('span');
     displayStreamTimerEl.innerHTML = `<span id="${streamTimerMonitorId}" style="font-size: 0.8em;">
-    &nbsp;開始: <input type="date" id="${startStreamDateId}"><input type="time" id="${startStreamTimeId}"><button type="button" id="${startStreamReserveButtonId}">予約</button><br />
-    &nbsp;終了: <input type="date" id="${endStreamDateId}"><input type="time" id="${endStreamTimeId}"><button type="button" id="${endStreamReserveButtonId}">予約</button>
-    </span>`;
+&nbsp;開始: <input type="date" id="${startStreamDateId}"><input type="time" id="${startStreamTimeId}"><button type="button" id="${startStreamReserveButtonId}">予約</button><br />
+&nbsp;終了: <input type="date" id="${endStreamDateId}"><input type="time" id="${endStreamTimeId}"><button type="button" id="${endStreamReserveButtonId}">予約</button>
+</span>`;
     displayStreamTimerEl.style.fontSize = '1.5em';
     displayStreamTimerEl.style.fontWeight = 'bold';
     displayStreamTimerEl.style.color = 'yellow';
     document.querySelector('.ytls-header.action-buttons').append(displayStreamTimerEl);
-    let now = new Date();
     let defaultYmd = now.getFullYear() + '-' + ('0' + (now.getMonth() + 1)).slice(-2) + '-' + ('0' + (now.getDate())).slice(-2);
     let defaultHm = now.toLocaleTimeString().substring(0, 5);
     document.getElementById(startStreamDateId).value = defaultYmd;
@@ -166,6 +233,13 @@ const DEFAULT_INTERVAL_MINUTES = 15;
     document.getElementById(endStreamTimeId).value = defaultHm;
     document.getElementById(startStreamReserveButtonId).addEventListener('click', scheduleStartStream);
     document.getElementById(endStreamReserveButtonId).addEventListener('click', scheduleEndStream);
+    if (!nowOnAir) {
+      document.getElementById(startStreamButtonId).addEventListener('click', function() {
+        // LIVE配信開始を直押しした場合にも広告自動挿入を起動する
+        document.getElementById(insertAdBaseDatetimeId).innerText = new Date().toLocaleString();
+        scheduleNextInsertAd();
+      });
+    }
   }
 
   setTimeout(initMe, 5 * 1000);
